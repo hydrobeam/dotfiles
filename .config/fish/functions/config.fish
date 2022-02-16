83486678e109b8a5746049357ed9d1083fe41fd7
Laith Bahodi <laithbahodi@gmail.com>
2022-02-16 17:53:12 -0500

    make fish use proper config fucntion

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,3 +1,3 @@
-function config --wraps='git --git-dir=/home/aquabeam/.dotfiles --work-tree=/home/aquabeam' --wraps='git --git-dir=$HOME/.dotfiles --work-tree=$HOME' --description 'config dotfiles setup'
-  git --git-dir=/home/aquabeam/.dotfiles --work-tree=/home/aquabeam $argv; 
+function config --wraps='git --git-dir=/home/aquabeam/.dotfiles --work-tree=/home/aquabeam' --wraps='git --git-dir=$HOME/.dotfiles --work-tree=$HOME' --description 'alias config git --git-dir=/home/aquabeam/.dotfiles --work-tree=/home/aquabeam'
+  git --git-dir=$HOME/.dotfiles --work-tree=$HOME $argv; 
 end
